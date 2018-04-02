3d86c111b619b5cd9a9a2763742029fc4c90e1bc
ericyork <eyork@clarkson.edu>
2018-04-01 23:16:57 -0400

    Revert "Commits this to revert"
    This reverts commit 4f86097d00651e0d0183fd85a426af8f37008d1a.

--- page.js
+++ page.js
@@ -33,16 +33,3 @@ function navToggle() {
     x.className = "topnav";
   }
 }
-//* Modal Scripts
-//* from a pen by Jack Zelig https://codepen.io/JackZelig/pen/ZLRZOY
-$(".button").on("click", function() {
-  var modal = $(this).data("modal");
-  $(modal).show();
-});
-
-$(".modal").on("click", function(e) {
-  var className = e.target.className;
-  if(className === "modal" || className === "close"){
-    $(this).closest(".modal").hide();
-  }
-});
